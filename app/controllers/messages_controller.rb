e51df9aaf8535654eaaaf7498e0587c21318e3ce
takacpa <t.ura0315@gmail.com>
2020-03-03 14:19:43 +0900

    modifying_add_ajax

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -12,8 +12,6 @@ class MessagesController < ApplicationController
       respond_to do |format|
         format.json
       end
-    # if @message.save
-    #   redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'
     else
       @messages = @group.messages.includes(:user)
       flash.now[:alert] = 'メッセージを入力してください。'
